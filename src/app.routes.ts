bb8ac60d07115403cb05d7f0586d190f62352439
ramraghav1 <ramshranlamichhane@gmail.com>
2026-02-21 17:18:02 +0545

    dashboard changed

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -16,7 +16,7 @@ export const appRoutes: Routes = [
         path: '',
         component: AppLayout,
         children: [
-            { path: '', component: Dashboard },
+            { path: 'my-project-dashboard', component: Dashboard },
             { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
             { path: 'documentation', component: Documentation },
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') },
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -4,16 +4,16 @@ import { StatsWidget } from './components/statswidget';
 import { RecentSalesWidget } from './components/recentsaleswidget';
 import { BestSellingWidget } from './components/bestsellingwidget';
 import { RevenueStreamWidget } from './components/revenuestreamwidget';
-
+import { MyprojectDashboard } from './components/myprojectdashboard';
 @Component({
     selector: 'app-dashboard',
-    imports: [StatsWidget, RecentSalesWidget, BestSellingWidget, RevenueStreamWidget, NotificationsWidget],
+    imports: [StatsWidget, RecentSalesWidget, BestSellingWidget, RevenueStreamWidget, NotificationsWidget, MyprojectDashboard],
     template: `
         <div class="grid grid-cols-12 gap-8">
             <app-stats-widget class="contents" />
             <div class="col-span-12 xl:col-span-6">
                 <app-recent-sales-widget />
-                <app-best-selling-widget />
+                <my-project-dashboard />
             </div>
             <div class="col-span-12 xl:col-span-6">
                 <app-revenue-stream-widget />
